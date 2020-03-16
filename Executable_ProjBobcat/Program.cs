e3649af0f7ad4fe67e6bc898ab57229dc54ed54a
Peter Ho <PupilStudio@outlook.com>
2020-03-16 21:09:37 +0800

    添加项目文件。

---
+++ Executable_ProjBobcat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml.Serialization;
+using ProjBobcat;
+
+namespace Executable_ProjBobcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
